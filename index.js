a0e5d06bc267fa3d9495960acb54c86767033b06
rumman <139452817+Rummantouqeer@users.noreply.github.com>
2023-10-10 07:45:21 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+import inquirer from "inquirer";
+import chalk from 'chalk';
+import showBanner from 'node-banner';
+(async () => {
+    await showBanner('Welcome to TODO', 'Plan your day effectively by organizing all your tasks in one place');
+})();
+let todo = [];
+let loop = true;
+while (loop) {
+    const answers = await inquirer.prompt([
+        {
+            type: "input",
+            name: "TODO",
+            message: "What do you want to add in your Todo?"
+        },
+        {
+            type: "confirm",
+            name: "addmore",
+            message: "Do you want to add more todo?",
+            default: false,
+        }
+    ]);
+    const { TODO, addmore } = answers;
+    console.log(chalk.bgBlueBright(answers));
+    loop = addmore;
+    if (TODO) {
+        todo.push(TODO);
+    }
+    else {
+        console.log(chalk.bgRedBright("kindly enter valid input"));
+    }
+    console.log(TODO);
+    if (todo.length > 0) {
+        console.log(chalk.bgBlueBright("Your todo list is:"));
+        todo.forEach(todo => {
+            console.log(chalk.bgBlueBright(todo));
+        });
+    }
+    else {
+        console.log(chalk.bgRedBright("No todo found"));
+    }
+}
